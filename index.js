bf782542af8cc66532f1dda70702244eef43d8a0
Silviu Glavan <silviuglv@gmail.com>
2021-01-29 13:35:41 +0200

    fix dest

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const mime = require('mime-types');
 
 const s3 = new S3();
 const source = core.getInput('source');
-const dest = core.getInput('dest');
+let dest = core.getInput('dest');
 const bucket = core.getInput('bucket');
+
+dest = dest === '/' ? '' : dest;
 
 function walkSync(dir, filelist) {
   var files = fs.readdirSync(dir);
